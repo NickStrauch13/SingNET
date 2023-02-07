931467a156012c54e7a69c4b7ea8ab7c873dda1d
nicholasconterno <nec21@duke.edu>
2023-02-07 14:58:52 -0500

    Got some spectogram, not really usable yet, but getting there.

--- SpectogramMaker.py
+++ SpectogramMaker.py
@@ -11,15 +11,16 @@ import IPython.display as ipd
 audio_fpath = "data/mp3/"
 audio_clips = os.listdir(audio_fpath)
 print("No. of .wav files in audio folder = ",len(audio_clips))
-x, sr = librosa.load(audio_fpath+audio_clips[2], sr=44100) ## ya this needs to be accurate
+## ya this needs to be accurate
 
-print(type(x), type(sr))
-print(x.shape, sr)
-plt.figure(figsize=(14, 5))
-librosa.display.waveshow(x, sr=sr)
-X = librosa.stft(x)
-Xdb = librosa.amplitude_to_db(abs(X))
-plt.figure(figsize=(14, 5))
-librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
-plt.colorbar()
-
+for i in range(len(audio_clips)):
+    # print(type(x), type(sr))
+    # print(x.shape, sr)
+    print(audio_clips[i])
+    x, sr = librosa.load(audio_fpath + audio_clips[i], sr=None)
+    X = librosa.stft(x)
+    Xdb = librosa.amplitude_to_db(abs(X))
+    plt.figure(figsize=(14, 5))
+    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
+    plt.colorbar()
+    plt.savefig("data/spectrogramImages/"+audio_clips[i].replace("mp3", "jpg"))
