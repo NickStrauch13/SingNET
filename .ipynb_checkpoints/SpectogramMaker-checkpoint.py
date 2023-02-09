6fed89ded2612b3a4b5238788141d7616041a4a6
Nick Strauch <njs35@duke.edu>
2023-02-09 13:52:05 -0500

    added robustness

--- .ipynb_checkpoints/SpectogramMaker-checkpoint.py
+++ .ipynb_checkpoints/SpectogramMaker-checkpoint.py
@@ -1,26 +1,58 @@
 import os
 import matplotlib.pyplot as plt
-
 #for loading and visualizing audio files
 import librosa
 import librosa.display
+from pydub import AudioSegment
+import time
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+def convertmp3sToWav(audio_clips):
+    audio_fpath = "data/costa_rica/mp3_windows/"
+    audio_clips = os.listdir(audio_fpath)
+    print("No. of .wav files in audio folder = ", len(audio_clips))
+    for i in range(len(audio_clips)):
+        # print(type(x), type(sr))
+        # print(x.shape, sr)
+        print(audio_clips[i])
+        if not audio_clips[i].endswith(".ipynb_checkpoints"):
+            tempPath = audio_fpath + audio_clips[i]
+            song = AudioSegment.from_mp3(tempPath)
+            song.export("data/costa_rica/wav/"+audio_clips[i].replace("mp3","wav"))
+def getSpectroFromWav(audio_clips):
+    for i in range(len(audio_clips)):
+        # print(type(x), type(sr))
+        # print(x.shape, sr)
+        print(audio_clips[i])
+        if not audio_clips[i].endswith(".ipynb_checkpoints"):
+            tempPath = audio_fpath + audio_clips[i]
+            x, sr = librosa.load(tempPath, sr=None)
+            # X = librosa.stft(x, n_fft=2048)
+            # Xdb = librosa.amplitude_to_db(abs(X))  ## Use These three lines for spectrogram
+            # librosa.util.normalize(Xdb)
+            Z = librosa.feature.melspectrogram(x,sr)
+            Zdb = librosa.amplitude_to_db(abs(Z))  ## Use these three lines for MEL spectrogram
+            librosa.util.normalize(Zdb)## I prefer MEL it is significantly faster and seems to give more interesting results
+            plt.figure(figsize=(14, 5))
+            librosa.display.specshow(Zdb, sr=sr, x_axis='time', y_axis='hz',cmap='viridis')
+            plt.title("")
+            plt.xlabel("")
+            plt.ylabel("")
+            plt.axis("off")
+            #plt.colorbar()
+            plt.savefig("data/costa_rica/spectrogram/"+audio_clips[i].replace("wav", "jpg"), bbox_inches="tight")
+            plt.close()
+            print(i*1.0/len(audio_clips))
+# audio_fpath = "data/costa_rica/wav/" #use this line for convertmp3sToWav
+audio_fpath = "data/costa_rica/wav/"  #use this line for getSpectroFromWav
 
-#to play audio
-import IPython.display as ipd
-
-audio_fpath = "data/mp3/"
 audio_clips = os.listdir(audio_fpath)
 print("No. of .wav files in audio folder = ",len(audio_clips))
-## ya this needs to be accurate
+t= time.time()
+#convertmp3sToWav(audio_clips)
+getSpectroFromWav(audio_clips)
 
-for i in range(len(audio_clips)):
-    # print(type(x), type(sr))
-    # print(x.shape, sr)
-    print(audio_clips[i])
-    x, sr = librosa.load(audio_fpath + audio_clips[i], sr=None)
-    X = librosa.stft(x)
-    Xdb = librosa.amplitude_to_db(abs(X))
-    plt.figure(figsize=(14, 5))
-    librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
-    plt.colorbar()
-    plt.savefig("data/spectrogramImages/"+audio_clips[i].replace("mp3", "jpg"))
+print(time.time()-t)
+
+
+
--- SpectogramMaker.py
+++ SpectogramMaker.py
@@ -8,36 +8,41 @@ import time
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 def convertmp3sToWav(audio_clips):
-    audio_fpath = "data/costa_rica/mp3/"
+    audio_fpath = "data/costa_rica/mp3_windows/"
     audio_clips = os.listdir(audio_fpath)
     print("No. of .wav files in audio folder = ", len(audio_clips))
     for i in range(len(audio_clips)):
         # print(type(x), type(sr))
         # print(x.shape, sr)
         print(audio_clips[i])
-        tempPath = audio_fpath + audio_clips[i]
-        print(tempPath)
-        song = AudioSegment.from_mp3(tempPath)
-        song.export("data/costa_rica/wav/"+audio_clips[i].replace("mp3","wav"))
+        if not audio_clips[i].endswith(".ipynb_checkpoints"):
+            tempPath = audio_fpath + audio_clips[i]
+            song = AudioSegment.from_mp3(tempPath)
+            song.export("data/costa_rica/wav/"+audio_clips[i].replace("mp3","wav"))
 def getSpectroFromWav(audio_clips):
     for i in range(len(audio_clips)):
         # print(type(x), type(sr))
         # print(x.shape, sr)
         print(audio_clips[i])
-        tempPath = audio_fpath + audio_clips[i]
-        x, sr = librosa.load(tempPath, sr=None)
-        # X = librosa.stft(x, n_fft=2048)
-        # Xdb = librosa.amplitude_to_db(abs(X))  ## Use These three lines for spectrogram
-        # librosa.util.normalize(Xdb)
-        Z = librosa.feature.melspectrogram(x,sr)
-        Zdb = librosa.amplitude_to_db(abs(Z))  ## Use these three lines for MEL spectrogram
-        librosa.util.normalize(Zdb)## I prefer MEL it is significantly faster and seems to give more interesting results
-        plt.figure(figsize=(14, 5))
-        librosa.display.specshow(Zdb, sr=sr, x_axis='time', y_axis='hz',cmap='viridis')
-        plt.colorbar()
-        plt.savefig("data/costa_rica/spectrogram/"+audio_clips[i].replace("wav", "jpg"))
-        plt.close()
-        print(i*1.0/len(audio_clips))
+        if not audio_clips[i].endswith(".ipynb_checkpoints"):
+            tempPath = audio_fpath + audio_clips[i]
+            x, sr = librosa.load(tempPath, sr=None)
+            # X = librosa.stft(x, n_fft=2048)
+            # Xdb = librosa.amplitude_to_db(abs(X))  ## Use These three lines for spectrogram
+            # librosa.util.normalize(Xdb)
+            Z = librosa.feature.melspectrogram(x,sr)
+            Zdb = librosa.amplitude_to_db(abs(Z))  ## Use these three lines for MEL spectrogram
+            librosa.util.normalize(Zdb)## I prefer MEL it is significantly faster and seems to give more interesting results
+            plt.figure(figsize=(14, 5))
+            librosa.display.specshow(Zdb, sr=sr, x_axis='time', y_axis='hz',cmap='viridis')
+            plt.title("")
+            plt.xlabel("")
+            plt.ylabel("")
+            plt.axis("off")
+            #plt.colorbar()
+            plt.savefig("data/costa_rica/spectrogram/"+audio_clips[i].replace("wav", "jpg"), bbox_inches="tight")
+            plt.close()
+            print(i*1.0/len(audio_clips))
 # audio_fpath = "data/costa_rica/wav/" #use this line for convertmp3sToWav
 audio_fpath = "data/costa_rica/wav/"  #use this line for getSpectroFromWav
 
